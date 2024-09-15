e6c79761376fddf0767254d2ed7ea98eebdc9a1c
seungju_hong <hongsj430@gmail.com>
2024-09-15 15:03:48 +0900

    add: project setting

---
+++ src/main/java/com/wildsoft/shopping_mall/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.wildsoft.shopping_mall;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ShoppingMallApplication.class);
+	}
+
+}
